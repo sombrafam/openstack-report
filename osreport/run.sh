25d1721483638fbf531f972f27918085402e2e96
Erlon R. Cruz <erlon@canonical.com>
2021-04-27 10:48:32 -0300

    Add openstack version to debug info

--- osreport/run.sh
+++ osreport/run.sh
@@ -57,7 +57,10 @@ EOF
 
 # x is a dummy value just so the script does not break when OS_REPORT_DEBUG is
 # not set.
-[[ -z ${OS_REPORT_DEBUG+x} ]] || set -x
+if [[ -z ${OS_REPORT_DEBUG+x} ]]; then
+    set -x
+    openstack --version
+fi
 
 plugins=${SUPPORTED_PLUGINS}
 while (($#)); do
